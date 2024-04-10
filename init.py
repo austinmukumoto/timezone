0fa3fac943130a9d5c251be8fc9891c132534cd4
AUSTIN JOSHUA MUKUMOTO <austinjoshuamukumoto@192.168.1.4>
2024-04-10 16:18:43 +0800

    fix: optimize logging

--- init.py
+++ init.py
@@ -11,22 +11,30 @@ class Timezone:
         message = None
         timeout = 30
         key = 'PVR9JSSQ0M6A'
+        base_url = 'http://api.timezonedb.com/v2.1'
        # API CALL FOR THE TIME ZONE LIST 
         try:
             resp_timezone_list = requests.post(
-                url='http://api.timezonedb.com/v2.1/list-time-zone',
+                url=f'{base_url}/list-time-zone',
                 params={
                     'key':key,
                     'format':'json'
                 },
                 timeout=timeout
             )
+            
+            if resp_timezone_list.json().get('status') != 'OK':
+                print('Error in pulling timezone list')
+                self.error_logging(resp_timezone_list.json().get('message'))
+                
+                return
 
-            # print(f'Timezone http status code response: {resp_timezone_list.status_code}')
-            # print(f'Timezone list response: {resp_timezone_list.json()}')
+            print(f'Timezone http status code response: {resp_timezone_list.status_code}')
+            print(f'Timezone list response: {resp_timezone_list.json()}')
 
             resp_data = resp_timezone_list.json().get('zones')
-                
+            
+        
             # CONVERT TO TUPLE FOR MULTPLE INSERTING
             datas = []
             zones = []
@@ -94,7 +102,7 @@ class Timezone:
                             # IMPLEMENT DELAY BECAUSE OF THE RATE LIMIT OF THE API
                             time.sleep(3)
                             resp_timezone_get =  requests.post(
-                                url='http://api.timezonedb.com/v2.1/get-time-zone',
+                                url=f'{base_url}/get-time-zone',
                                 params={
                                     'key':key,
                                     'by':'zone',
@@ -103,10 +111,11 @@ class Timezone:
                                 },
                                 timeout=timeout
                             )
+                            print(f'Timezone http status code response: {resp_timezone_get.status_code}')
+                            print(f'Timezone list response: {resp_timezone_get.json()}')
+                            
                             
                             resp_data = resp_timezone_get.json()
-                            print(resp_timezone_get.text)
-                            print(resp_data)
 
                             # NO.1 PHASE - 2 POPULATE TO THE TZDB_ZONES_DETAILS DATA
                             query = "INSERT INTO `TZDB_ZONES_DETAILS` " \
@@ -132,9 +141,6 @@ class Timezone:
                             conn.commit()  
                             
                             count+=1
-                                    
-                                   
-         
                 
         except Exception as e:
             # NO.2 FOR REPLICATION OF THIS EXERCISE I SET UP TIMEOUT SETTINGS ON THE API CALL , to 0.1 to Achieved the timeout scenario
@@ -145,14 +151,18 @@ class Timezone:
                 message = 'Timeout Request'
                 # ERROR LOG SAVING
             
-            with db.get_database() as conn:
-                query = "INSERT INTO `TZDB_ERROR_LOG` " \
+                self.error_logging(message)
+                    
+                       
+    def error_logging(self,message):
+        with db.get_database() as conn:
+            query = "INSERT INTO `TZDB_ERROR_LOG` " \
                             "(`ERROR_DATE`, `ERROR_MESSAGE`) " \
                             "VALUES (%s, %s)"
-                with conn.cursor() as cursor:
-                    cursor.execute(query,
-                                (datetime.now(),message))
-                    conn.commit()    
+            with conn.cursor() as cursor:
+                cursor.execute(query,
+                            (datetime.now(),message))
+                conn.commit()          
                         
 # EXECUTION 
 Timezone()
